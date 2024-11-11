6d771698cda0fa8216d81207e99a39ccbffa18b1
Ramazan Fehmi Ay <91493055+Escanorrrr@users.noreply.github.com>
2024-11-11 20:04:54 +0300

    Update main.py
    Sözlüğüme bir kelime daha ekledim .

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
+            "COOL": "Havalı olmak ya da benzeri bir şey "
+            
             }
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 
